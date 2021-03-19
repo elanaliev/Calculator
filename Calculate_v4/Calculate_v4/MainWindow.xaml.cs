9b3ea445e7e27dd49f017dfb68cb03f836b984f9
elanaliev <52862362+elanaliev@users.noreply.github.com>
2021-03-19 15:35:54 +0300

    update
    int => double
    add converting ( sqrt, round, cos )
    add empty field check
    add negative numbers

--- Calculate_v4/Calculate_v4/MainWindow.xaml.cs
+++ Calculate_v4/Calculate_v4/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace Calculate_v4
 {
@@ -29,9 +30,9 @@ namespace Calculate_v4
         int opState = 0;
         Dictionary<string, string[]> Operators = new Dictionary<string, string[]>() { 
             { "a1", new string[] {"+", "^"} }, 
-            { "a2", new string[] { "-", "" } }, 
-            { "a3", new string[] { "*", "" } }, 
-            { "a4", new string[] { "/", "" } } 
+            { "a2", new string[] { "-", "√" } }, 
+            { "a3", new string[] { "*", "R" } }, 
+            { "a4", new string[] { "/", "cos" } } 
         };
 
         #region Main Controls
@@ -46,34 +47,76 @@ namespace Calculate_v4
             MainTBox.Text = (MainTBox.Text == "0") ? button.Content.ToString() : MainTBox.Text + button.Content;
         }
 
+
+        #region DRY? nope.
         private void ActionButton_Click(object sender, RoutedEventArgs e)
         {
             var button = (Button)sender;
             int operatorIndex = ATBox.Text.Length - 1;
-            string result = ATBox.Text;
-            result = (result == string.Empty) ? 
-                MainTBox.Text + button.Content : Solve(result[operatorIndex], int.Parse(result.Substring(0, operatorIndex)), int.Parse(MainTBox.Text)).ToString() + button.Content;
-            ATBox.Text = result;
-            MainTBox.Text = string.Empty;
+            string expr = ATBox.Text;
+            string mtext = MainTBox.Text;
+
+            if (double.TryParse(mtext, out var a) == true)
+            {
+                if(!IsConverting(button.Content.ToString(), mtext))
+                {
+                    expr = (expr == string.Empty) ? MainTBox.Text + button.Content : Solve(expr[operatorIndex], double.Parse(expr.Substring(0, operatorIndex)), double.Parse(mtext)).ToString() + button.Content;
+                    ATBox.Text = expr;
+                    MainTBox.Text = string.Empty;
+                }
+            }
+            else
+            {
+                if (button.Content.ToString() == "-")
+                {
+                    MainTBox.Text = (mtext.Contains("-")) ? mtext : mtext + '-';
+                }
+            }
+            opState = 0;
+            SwitchOperators();
         }
 
         private void AnswerButton_Click(object sender, RoutedEventArgs e)
         {
             int operatorIndex = ATBox.Text.Length - 1;
-            string result = ATBox.Text;
-            result = Solve(result[operatorIndex], int.Parse(result.Substring(0, operatorIndex)), int.Parse(MainTBox.Text)).ToString();
+            string expr = ATBox.Text;
+            string mtext = MainTBox.Text;
+            expr = (expr == string.Empty) ?  expr : Solve(expr[operatorIndex], double.Parse(expr.Substring(0, operatorIndex)), (double.TryParse(mtext, out var a)) ? a : 0).ToString();
             ATBox.Text = string.Empty;
-            MainTBox.Text = result;
+            MainTBox.Text = expr;
         }
+        #endregion
 
-        int Solve(char _operator, int val1, int val2)
+        double Solve(char _operator, double val1, double val2)
         {
             if (_operator == '+') return val1 + val2;
             if (_operator == '-') return val1 - val2;
             if (_operator == '*') return val1 * val2;
             if (_operator == '/') return val1 / ((val2 != 0) ? val2 : 1);
-            if (_operator == '^') return val1 ^ val2;
+            if (_operator == '^') return Math.Pow(val1, val2);
             else return 0;
+        }
+
+        bool IsConverting(string type, string val)
+        {
+            double value = double.Parse(val);
+            if (type == "√")
+            {
+                MainTBox.Text = Math.Sqrt(value).ToString();
+                return true;
+            }
+            if(type == "R")
+            {
+                MainTBox.Text = Math.Round(value).ToString();
+                return true;
+            }
+            if(type == "cos")
+            {
+                MainTBox.Text = Math.Cos(value).ToString();
+                return true;
+            }
+            else return false;
+            
         }
 
         void SwitchOperators()
